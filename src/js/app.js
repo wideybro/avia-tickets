ecabf87bee7730da4ab2851e83129a8b67b3419a
ARTEM BABENKO <ghetto.workout.gw@gmail.com>
2020-10-30 19:59:30 +0300

    feat: render tickets

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import '../css/style.css';
 import './plugins';
 import locations from './store/locations';
 import formUI from './views/form';
+import ticketsUI from './views/tickets';
 import currencyUI from './views/currency';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -35,5 +36,8 @@ document.addEventListener('DOMContentLoaded', () => {
             return_date,
             currency,
         });
+
+        console.log(locations.lastSearch);
+        ticketsUI.renderTickets(locations.lastSearch);
     }
 })
--- src/js/views/currency.js
+++ src/js/views/currency.js
@@ -1,10 +1,18 @@
 class CurrencyUI {
     constructor() {
         this.currency = document.getElementById('currency');
+        this.dictionary = {
+            USD: '$',
+            EUR: '€'
+        }
     }
 
     get currencyValue() {
         return this.currency.value;
+    }
+
+    getСurrencySymbol() {
+        return this.dictionary[this.currencyValue];
     }
 }
 
